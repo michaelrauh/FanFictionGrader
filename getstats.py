37e0f61166b4ad545dc71d827df83a5f6d60e3f0
Michael Rauh <michaelrauh@ymail.com>
2013-07-28 22:17:50 -0400

    Words not in dictionary search improved

--- getstats.py
+++ getstats.py
@@ -1,4 +1,5 @@
 import math
+import string
 
 #read file into string
 def read_to_string(filename):
@@ -16,26 +17,19 @@ def avg_sentence_length(document):
     return words/periods
 
 word_list = read_to_list('data/wordlist.txt') #read dictionary
-#returns unique words not in dictionary
+#returns unique words not in dictionary-This is currently catching proper nouns
 def notfound (words,word_list = word_list):
-    not_in_dictionary = []
-    for word in words:
-        posessive ="'s"
-        word = word.strip(posessive)
-        word = word.strip('-.,:?\'\"!').lower()
-        if word not in word_list:
-            not_in_dictionary.append(word)
-    return list(set(not_in_dictionary))
+    for i in range(0,len(words)):
+        strip_words = "'s" + string.punctuation
+        words[i] = words[i].strip(strip_words).lower()
+    return set(words).difference(word_list)
     
 spam_files = read_to_list('data/fics/badhp.txt')
 ham_files = read_to_list('data/fics/goodhp.txt')
 
-count = 0
 for i in spam_files:
-    count +=1
     document = read_to_string('data/fics/' + i)
     average_sentence_length = avg_sentence_length(document)
     fic_length = len(document)
     words = document.split()
     not_in_dictionary = notfound(words)
-    print(not_in_dictionary)
