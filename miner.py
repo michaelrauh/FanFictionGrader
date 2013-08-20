8bcb7dccae883909902083b3dc062330a06f115d
Michael Rauh <michaelrauh@ymail.com>
2013-08-20 00:18:54 -0400

    miner gets titles instead of dates, as dates
    Dates are omitted on site if there are no changes. Titles are
    nonstandard, so it may be necessary to strip unwanted characters later.

--- miner.py
+++ miner.py
@@ -2,7 +2,7 @@ import urllib2
 links = []
 favs = []
 names=[]
-dates =[]
+titles =[]
 def tothe (x):
     return x**1.28
 for i in map(tothe,range(1,2000)):
@@ -10,7 +10,7 @@ for i in map(tothe,range(1,2000)):
     page_links = []
     page_favs = []
     page_names = []
-    page_dates = []
+    page_titles = []
     print(i)
     response = urllib2.urlopen('http://www.fanfiction.net/book/Harry-Potter/?&srt=4&lan=1&r=10&p=' + str(i))
     html = response.read()
@@ -27,38 +27,30 @@ for i in map(tothe,range(1,2000)):
     for word in range (0,len(words)-1):
         if words[word].find('Favs:') != -1:
             page_favs.append(words[word+1])
-    for word in range (0,len(words)-1):
-        if words[word].find('Updated:') != -1:
-            temp = words[word+1].split('-')
-            for i in range (0,len (temp)):
-                if len(temp[i]) < 2:
-                    temp[i] = '0' + temp[i]
-            temp = ''.join(temp)
-            if temp[-2:] != 99:
-                temp = str(temp[:-2]) + str(20) + str(temp[-2:])
-            else:
-                temp = str(temp[:-2]) + str(19) + str(temp[-2:])
-            page_dates.append(temp)
+    for word in range (0,len(words2)-2):
+        if words2[word].find('title') != -1 :
+            if words2[word+2][-3:] == '</a':
+                page_titles.append(words2[word+2][:-3])
     for word in range (0,len(words2)):
         if words2[word].find('href="/u')!=-1:
             temp = words2[word+1]
             page_names.append(temp[:temp.find('<')])
-    if len(page_links) == len(page_favs) == len(page_names) == len(page_dates): # sometimes formatting on links is weird and one is skipped. If this happens the page is thrown out
+    if len(page_links) == len(page_favs) == len(page_names) == len(page_titles): # sometimes formatting on links is weird and one is skipped. If this happens the page is thrown out
         links += page_links
         favs += page_favs
         names += page_names
-        dates += page_dates
-f = open("links.txt",'w')
+        titles += page_titles
+f = open("data/links.txt",'w')
 for link in links:
     f.write(link)
     f.write('\n')
 f.close()
-f = open("names.txt",'w')
+f = open("data/names.txt",'w')
 for j in range (0,len(favs)):
     f.write(favs[j])
     f.write('\n')
     f.write(names[j])
     f.write('-')
-    f.write(dates[j])
+    f.write(titles[j])
     f.write('\n')
 f.close()
