e79516f32eab06bb7f5d3c1281e7e0ce2b4f3d52
Michael Rauh <michaelrauh@ymail.com>
2013-08-26 18:07:42 -0400

    Hidden nodes and validation numbers selected

--- train.py
+++ train.py
@@ -11,18 +11,18 @@ for i in range (0,len(targets)):
     targets[i] = float(''.join(targets[i].split(',')))
 
 dim = len(inpts[0])
-for num_hidden in range(1,80):
-    net = buildNetwork(dim,num_hidden,1,bias=True)
-    ds = SupervisedDataSet(dim,1)
+num_hidden = 65
+net = buildNetwork(dim,num_hidden,1,bias=True)
+ds = SupervisedDataSet(dim,1)
 
-    for i in range (0,len(inpts),400):
-        ds.addSample(inpts[i],targets[i])
+for i in range (0,len(inpts),1):
+    ds.addSample(inpts[i],targets[i])
 
-    trainer = BackpropTrainer(net,ds)
-    begin = time.time()
-    out = trainer.trainUntilConvergence(maxEpochs=1000) #dataset=None, maxEpochs=None, verbose=None, continueEpochs=10, validationProportion=0.25
-    end = time.time()
-    print 'hidden:',num_hidden
-    print 'Time:',end-begin
-    print 'Error:',min(out[1]) # minimum error
-    print ''
+trainer = BackpropTrainer(net,ds)
+begin = time.time()
+out = trainer.trainUntilConvergence(maxEpochs=10000,validationProportion=0.05,continueEpochs=100) #dataset=None, maxEpochs=None, verbose=None, continueEpochs=10, validationProportion=0.25
+end = time.time()
+print 'hidden:',num_hidden
+print 'Time:',end-begin
+print 'Error:',min(out[1]) # minimum error
+print ''
